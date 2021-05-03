6602feaa119a4f2343c7654b300b6d741e3558f2
Ilya Rezanov <ilya.shcoder@gmail.com>
2021-05-03 22:34:32 +0300

    [feat]: update the card component by adding some logic

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,7 +1,23 @@
-import './Card.scss';
+import "./Card.scss";
 
-function Card() {
-
+function Card(props) {
+	const { number, title, description, sourceTitle, source } = props;
+	return (
+		<div className="card">
+			<div className="card__number">{number}</div>
+			<div className="card__content">
+				<h3 className="card__title">{title}</h3>
+				<p className="card__description">{description}</p>
+				{source ? (
+					<p className="card__source">
+						<a href="source" className="card__link">
+							{sourceTitle}
+						</a>
+					</p>
+				) : null}
+			</div>
+		</div>
+	);
 }
 
 export default Card;
