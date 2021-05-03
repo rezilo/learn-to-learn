fa3c8be8dd8c932ef2a20ee78c5f3623788dd39f
Ilya Rezanov <ilya.shcoder@gmail.com>
2021-05-03 08:27:17 +0300

    [feat]: adding title component in root app file and update basic styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.scss';
+import Title from './Components/Title/Title'
 
 function App() {
-
+  return (
+    <div className="app">
+      <Title />
+    </div>
+  );
 }
 
 export default App;
