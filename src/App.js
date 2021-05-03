41173ddf2640c86e35ed777ccb68d95d1d21520f
Ilya Rezanov <ilya.shcoder@gmail.com>
2021-05-03 11:50:13 +0300

    [feat]: update basic application styles and template layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
-
+	return (
+		<div className="app app--background">
+			<div className="app--content"></div>
+		</div>
+	);
 }
 
 export default App;
