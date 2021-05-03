3e314fade1da22bd9843cba63a3124d0c8405b12
Ilya Rezanov <ilya.shcoder@gmail.com>
2021-05-03 15:13:18 +0300

    [fix]: finalize component v.0.0.1

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,18 +1,18 @@
-import './Title.scss';
+import "./Title.scss";
 
 function Title(props) {
-  return (
-    <div className="title-component">
-      <h1 className="title-component__main-title">Учись учиться</h1>
-      <h2 className="title-component__main-subtitle">
-        Нам необходимо учиться чтобы оставаться востребованными в любой сфере деятельности. <br />
-        Но как это делать эффективно? Для начала нужно научиться учиться! <br/>
-      </h2>
-      <h3 className="title-component__description">
-        Ниже список из 10 техник которые позволяют мне вот уже семь лет делать это эффективно.
-      </h3>
-    </div>
-  )
+	return (
+		<div className="title-component">
+			<h1 className="title-component__main-title">Учись учиться</h1>
+			<h2 className="title-component__main-subtitle">
+				Нам необходимо учиться чтобы оставаться востребованными в любой сфере деятельности. <br />
+				Но как это делать эффективно? Для начала нужно научиться учиться! <br />
+			</h2>
+			<h3 className="title-component__description">
+				Ниже список из 10 техник которые позволяют мне вот уже пять лет делать это эффективно.
+			</h3>
+		</div>
+	);
 }
 
 export default Title;
