0b420d0d1eab52b49e13408485d1935e5004be14
Ilya Rezanov <ilya.shcoder@gmail.com>
2021-05-07 21:53:03 +0300

    [feat]: adding content translation in english

--- src/assets/localization.js
+++ src/assets/localization.js
@@ -102,4 +102,107 @@ export const messages = {
 			},
 		],
 	},
+	en: {
+		title: "Learn to learn",
+		source: "To learn more: ",
+		mainTitle:
+			"We need to learn to stay in demand in any field of activity. But how can this be done effectively? First you need to learn how to learn!",
+		mainSubtitle:
+			"Below is a list of 10 techniques that have allowed me to do this effectively for seven years now.",
+		rules: [
+			{
+				title: "Collection of information",
+				description:
+					"With a probability of 99% - people have already gone a similar path. At this stage, it is important to understand the scope and structure of the upcoming study. The task is to gather information and learn to distinguish good from bad.",
+				source: [
+					"https://informjournal.ru/kak-bystro-osvoit-novyj-navyk-ili-izuchit-novyj-material/",
+					"https://zapier.com/blog/learning-new-skills/",
+					"https://medium.com/skilluped/your-essential-guide-to-learn-new-skills-fast-c7e3fc493bea",
+				],
+			},
+			{
+				title: "Planning",
+				description:
+					"When planning, a difficult task is divided into small steps. This will allow you not to panic at the seeming impossibility of achieving the goal. Roadmap is a suitable option, but don't use ready-made ones! Make your own, even if you took someone else's as a basis. The task is to figure out and understand why the training sequence is built this way.",
+				source: [
+					"https://navlasov.livejournal.com/793.html",
+					"https://4brain.ru/blog/%D1%8D%D1%84%D1%84%D0%B5%D0%BA%D1%82%D0%B8%D0%B2%D0%BD%D0%BE%D0%B5-%D0%BF%D0%BB%D0%B0%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/",
+					"https://ru.wikipedia.org/wiki/Getting_Things_Done",
+					"",
+				],
+			},
+			{
+				title: "Actualization",
+				description:
+					"After collecting information and drawing up a plan, synchronize one with the other. Add what is missing and feel free to throw away the excess. After that, make a study schedule. Most importantly, remember that it is better a little, but regularly, than a lot at a time.",
+				source: [
+					"https://medium.com/skilluped/how-to-constantly-learn-new-skills-a-beginners-guide-to-your-upskilling-journey-fdd3d98227ac",
+				],
+			},
+			{
+				title: "Pen and notepad",
+				description:
+					"Despite the convenience of taking notes in digital format, fixing information on paper more effectively affects memorization. Here is research. The main thing is not to do it thoughtlessly. To begin with, it is worth choosing a method of taking notes, and then watching, reading or listening to training materials and abstracts the key ideas from them from memory.",
+				source: [
+					"https://habr.com/ru/post/393099/",
+					"https://www.npr.org/2016/04/17/474525392/attention-students-put-your-laptops-away",
+					"",
+				],
+			},
+			{
+				title: "Don't be afraid to refuse",
+				description:
+					"Studying is stressful. If along the way you feel that there is something you do not like about the material - refuse. Interference in the perception of information will not help assimilation. You'd better find an alternative.",
+				source: [
+					"https://4brain.ru/blog/stress-in-learning/",
+					"https://www.verywellmind.com/top-school-stress-relievers-for-students-3145179",
+					"https://www.kaptest.com/study/gmat/6-ways-to-decrease-stress-from-studying/",
+				],
+			},
+			{
+				title: "Study from different angles",
+				description:
+					"Sometimes the scope of lectures, lessons or articles is limited. Expand the topic yourself, so you will understand it deeper. Try to find additional sources of information on each new topic.",
+				source: ["https://habr.com/ru/post/499224/"],
+			},
+			{
+				title: "Practice, practice and more ...",
+				description:
+					"Practice as soon as possible. It may seem that there is not enough knowledge yet and whatever. It only seems to be. Practice> theory.",
+				source: [
+					"https://listdose.co/10-reasons-practical-education-more-important-theoretical/",
+					"https://www.theemotionmachine.com/practice-is-more-important-than-theory/",
+				],
+			},
+			{
+				title: "Repeat",
+				description:
+					"The learning curve clearly shows how quickly our brains clean up debris to avoid wasting energy. Use it for good - repeat.",
+				source: [
+					"https://en.wikipedia.org/wiki/Spaced_repetition",
+					"https://4brain.ru/blog/interval-repeat-method/",
+					"https://habr.com/ru/company/productivity_inside/blog/322286/",
+				],
+			},
+			{
+				title: "Second brain",
+				description:
+					"This point is more about advanced techniques. The knowledge base is a wikipedia on a computer that can be accessed both locally and via the Internet. At the initial stage, this takes a lot of time and effort. Only with time and with the growth of the base do you understand the benefits and power of this approach. It's about contextually linking notes to each other. For me personally, at the initial stage, it helped to see the big picture of the technologies that I studied. There was a mess of unrelated information in my head. After linking the notes with the context, the picture was formed. Also, the main advantage of your own knowledge base is the ability to independently build a structure for storing and maintaining notes.",
+				source: [
+					"https://www.youtube.com/watch?v=OP3dA2GcAh8",
+					"https://www.keepproductive.com/blog/how-to-build-a-second-brain",
+				],
+			},
+			{
+				title: "Rest regularly",
+				description:
+					"Sleep is just as important as learning materials. To find out how to improve your sleep quality, I recommend this site. I was inspired by him to create this page. Lack of sleep affects not only the physical, but also the mental state.",
+				source: [
+					"https://www.brightdevelopers.com/sleep-affects-learning/",
+					"https://www.sleep.org/sleep-affects-memory-learning/",
+					"https://healthysleep.med.harvard.edu/healthy/matters/benefits-of-sleep/learning-memory",
+				],
+			},
+		],
+	},
 };
