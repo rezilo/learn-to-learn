e84c4123aafea36175e79ee29601464d6fa77f1a
Ilya Rezanov <ilya.shcoder@gmail.com>
2021-05-07 17:24:38 +0300

    [fix]: added missing commas

--- src/assets/localization.js
+++ src/assets/localization.js
@@ -12,36 +12,38 @@ export const messages = {
 				description:
 					"С вероятностью 99% - люди уже прошли аналогичный путь. На этом этапе важно понять объем и структуру грядущей учебы.  Задача - собрать информацию и научиться отличать хорошую от плохой.",
 				source: [
-					'https://informjournal.ru/kak-bystro-osvoit-novyj-navyk-ili-izuchit-novyj-material/',
-					'https://zapier.com/blog/learning-new-skills/',
-					'https://medium.com/skilluped/your-essential-guide-to-learn-new-skills-fast-c7e3fc493bea',
-				]
+					"https://informjournal.ru/kak-bystro-osvoit-novyj-navyk-ili-izuchit-novyj-material/",
+					"https://zapier.com/blog/learning-new-skills/",
+					"https://medium.com/skilluped/your-essential-guide-to-learn-new-skills-fast-c7e3fc493bea",
+				],
 			},
 			{
 				title: "Планирование",
 				description:
 					"При планировании сложная задача делится на маленькие шаги. Это позволит не впасть в панику при кажущейся невозможности достижения цели. Дорожная карта – подходящий вариант, но не используй готовые! Составь собственную, даже если за основу взял чужую. Задача – разобраться и понять, почему последовательность обучения строится именно так.",
 				source: [
-					'https://navlasov.livejournal.com/793.html',
-					'https://4brain.ru/blog/%D1%8D%D1%84%D1%84%D0%B5%D0%BA%D1%82%D0%B8%D0%B2%D0%BD%D0%BE%D0%B5-%D0%BF%D0%BB%D0%B0%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/'
-					'https://ru.wikipedia.org/wiki/Getting_Things_Done',
-					''
+					"https://navlasov.livejournal.com/793.html",
+					"https://4brain.ru/blog/%D1%8D%D1%84%D1%84%D0%B5%D0%BA%D1%82%D0%B8%D0%B2%D0%BD%D0%BE%D0%B5-%D0%BF%D0%BB%D0%B0%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/",
+					"https://ru.wikipedia.org/wiki/Getting_Things_Done",
+					"",
 				],
 			},
 			{
 				title: "Актуализация",
 				description:
 					"После сбора информации и составления плана – синхронизируй одно с другим. Добавь недостающее и смело выбрасывай лишнее. После этого, составь график учебы. Главное, помни, что лучше по чуть-чуть, но регулярно, чем много и за раз.",
-				source: ['https://medium.com/skilluped/how-to-constantly-learn-new-skills-a-beginners-guide-to-your-upskilling-journey-fdd3d98227ac'],
+				source: [
+					"https://medium.com/skilluped/how-to-constantly-learn-new-skills-a-beginners-guide-to-your-upskilling-journey-fdd3d98227ac",
+				],
 			},
 			{
 				title: "Ручка и блокнот",
 				description:
 					"Не смотря на удобство ведения заметок в цифровом формате, фиксация информации на бумаге эффективнее влияет на запоминание. Главное, не делать это бездумно. Для начала стоит выбрать метод ведения конспекта, а затем смотреть, читать или слушать обучающие материалы и тезисно фиксировать ключевые идеи из них по памяти.",
 				source: [
-					'https://habr.com/ru/post/393099/',
-					'https://www.npr.org/2016/04/17/474525392/attention-students-put-your-laptops-away',
-					''
+					"https://habr.com/ru/post/393099/",
+					"https://www.npr.org/2016/04/17/474525392/attention-students-put-your-laptops-away",
+					"",
 				],
 			},
 			{
@@ -49,24 +51,24 @@ export const messages = {
 				description:
 					"Учеба – это серьезный стресс. Если по ходу дела чувствуешь, что что-то не нравится в материале – откажись. Помехи в восприятии информации не помогут усвоению. Лучше найди альтернативу.",
 				source: [
-					'https://4brain.ru/blog/stress-in-learning/',
-					'https://www.verywellmind.com/top-school-stress-relievers-for-students-3145179'
-					'https://www.kaptest.com/study/gmat/6-ways-to-decrease-stress-from-studying/'
+					"https://4brain.ru/blog/stress-in-learning/",
+					"https://www.verywellmind.com/top-school-stress-relievers-for-students-3145179",
+					"https://www.kaptest.com/study/gmat/6-ways-to-decrease-stress-from-studying/",
 				],
 			},
 			{
 				title: "Разносторонний взгляд",
 				description:
 					"Иногда рамки лекций, уроков или статей ограничены. Раскрывай тему сам, так глубже разберешься. Старайся на каждую новую тему находить дополнительные источники информации.",
-				source: ['https://habr.com/ru/post/499224/'],
+				source: ["https://habr.com/ru/post/499224/"],
 			},
 			{
 				title: "Практикуйтесь",
 				description:
 					"Практикуйся как можно скорее. Может казаться, что еще не достаточно знаний и что угодно еще. Это только кажется. Практика > теории.",
 				source: [
-					'https://listdose.co/10-reasons-practical-education-more-important-theoretical/',
-					'https://www.theemotionmachine.com/practice-is-more-important-than-theory/'
+					"https://listdose.co/10-reasons-practical-education-more-important-theoretical/",
+					"https://www.theemotionmachine.com/practice-is-more-important-than-theory/",
 				],
 			},
 			{
@@ -74,9 +76,9 @@ export const messages = {
 				description:
 					"Кривая обучения четко показывает, как быстро наш мозг чистит мусор, чтобы не тратить лишнюю энергию. Используй это во благо – повторяй.",
 				source: [
-					'https://en.wikipedia.org/wiki/Spaced_repetition',
-					'https://4brain.ru/blog/interval-repeat-method/',
-					'https://habr.com/ru/company/productivity_inside/blog/322286/'
+					"https://en.wikipedia.org/wiki/Spaced_repetition",
+					"https://4brain.ru/blog/interval-repeat-method/",
+					"https://habr.com/ru/company/productivity_inside/blog/322286/",
 				],
 			},
 			{
@@ -84,8 +86,8 @@ export const messages = {
 				description:
 					"Этот пункт относится скорее к продвинутым техникам. База знаний – это википедия на компьютере, к которой есть доступ как локально так и через интернет. На начальном этапе это отнимает много времени и сил. Только со временем и с ростом базы понимаешь пользу и мощь этого подхода. Суть в контекстуальном связывании заметок друг с другом. Лично мне на начальном этапе это помогло увидеть общую картину технологий, которые я изучал. В голове был бардак из несвязанной информации. После связывания заметок контекстом – картинка сложилась. Также главный плюс собственной базы знаний возможность самостоятельно выстраивать структуру хранения и ведения заметок.",
 				source: [
-					'https://www.youtube.com/watch?v=OP3dA2GcAh8',
-					'https://www.keepproductive.com/blog/how-to-build-a-second-brain',
+					"https://www.youtube.com/watch?v=OP3dA2GcAh8",
+					"https://www.keepproductive.com/blog/how-to-build-a-second-brain",
 				],
 			},
 			{
@@ -93,9 +95,9 @@ export const messages = {
 				description:
 					"Сон не менее важен, чем материалы во время учебы. Чтобы узнать как улучшить качество сна, рекомендую этот сайт. Я вдохновился им на создание этой странички. Недосып не только сказывается на физическом, но и на ментальном состоянии.",
 				source: [
-					'https://www.brightdevelopers.com/sleep-affects-learning/',
-					'https://www.sleep.org/sleep-affects-memory-learning/',
-					'https://healthysleep.med.harvard.edu/healthy/matters/benefits-of-sleep/learning-memory'
+					"https://www.brightdevelopers.com/sleep-affects-learning/",
+					"https://www.sleep.org/sleep-affects-memory-learning/",
+					"https://healthysleep.med.harvard.edu/healthy/matters/benefits-of-sleep/learning-memory",
 				],
 			},
 		],
