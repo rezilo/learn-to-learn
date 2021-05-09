48de3c260953a8cf42b4286f2600cc711bc2d4b0
Ilya Rezanov <62277472+rezilo@users.noreply.github.com>
2021-05-09 17:53:00 +0300

    Styles/layout (#9)
    * [feat]: finalize app layout and color scheme +update localization
    
    * [fix]: mobile layout fixes + font-size corrects
    
    * [styles]: update styles and layout of card component
    
    * [feat]: added icon in rule list
    
    * [fix]: remove react logo
    
    * [styles]: update styles for component
    
    * [fix]: remove service code

--- src/Components/Wave/Wave.js
+++ src/Components/Wave/Wave.js
@@ -3,12 +3,8 @@ import "./Wave.scss";
 function Wave() {
 	return (
 		<div className="wave">
-			<svg
-				viewBox="0 0 1440 120"
-				xmlns="http://www.w3.org/2000/svg"
-				xmlnsXlink="http://www.w3.org/1999/xlink"
-			>
-				<linearGradient id="linear-gradient">
+			<svg viewBox="0 0 1440 120" xmlns="http://www.w3.org/2000/svg">
+				<linearGradient id="linear-gradient" gradientTransform="rotate(0)">
 					<stop offset="0%" stopColor="#fad0c4" />
 					<stop offset="100%" stopColor="#ffd1ff" />
 				</linearGradient>
@@ -16,7 +12,7 @@ function Wave() {
 					fill="url(#linear-gradient)"
 					d="M 0 0 C 197.5 0 197.5 97 395 97 L 395 97 L 395 0 L 0 0 Z M 394 97 C 556 97 556 25 718 25 L 718 25 L 718 0 L 394 0 Z M 717 25 C 902 25 902 114 1087 114 L 1087 114 L 1087 0 L 717 0 Z M 1086 114 C 1263 114 1263 0 1440 0 L 1440 0 L 1440 0 L 1086 0 Z"
 					strokeWidth="0"
-				></path>{" "}
+				></path>
 			</svg>
 		</div>
 	);
