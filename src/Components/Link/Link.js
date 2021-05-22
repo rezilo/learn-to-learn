71e90ad72a8e1952cad18ccf4a5bda0ab73d7a48
Ilya Rezanov <62277472+rezilo@users.noreply.github.com>
2021-05-22 16:25:44 +0300

    [fix]: now all links to source open in new window (#13)

--- src/Components/Link/Link.js
+++ src/Components/Link/Link.js
@@ -3,7 +3,7 @@ import "./Link.scss";
 function Link(props) {
 	return props.links.map((link, i) => {
 		return (
-			<a href={link} key={i} className="card__link">
+			<a href={link} target="_blank" key={i} className="card__link">
 				{props.linkWrapper}
 			</a>
 		);
